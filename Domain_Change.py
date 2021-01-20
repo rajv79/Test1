8107d90ba3769cfccaa56f87d41077578b9ad0ed
Vivek Raj <rajviv79@gmail.com>
2021-01-20 13:27:30 -0500

    Domain_Change_modifiyed

--- Domain_Change.py
+++ Domain_Change.py
@@ -4,4 +4,6 @@ def replace_domain(email,old_domain,new_domain):
         new_email = email[:index]+"@"+ new_domain
         return new_email
     return email
-    
+
+print("Enter your info")
+print(replace_domain("rajv79@gmail.com","yahoo.com","hotmail.com"))
